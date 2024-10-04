6aaac0f26cd80a92d556727f29f134477c25c43d
Craby <lethuy1137@gmail.com>
2024-10-04 20:20:26 +0800

    update SQL file

--- miniproject-backend/init.sql
+++ miniproject-backend/init.sql
@@ -2,18 +2,26 @@ create table if not exists form
 (
     full_name     varchar(255) not null,
     email         varchar(255) not null
-        primary key,
+    primary key,
     phone_number  int          not null,
     country       varchar(255) not null,
     gender        int          not null,
     qualification text         not null
-);
+    );
+
+create table if not exists user_token
+(
+    email       varchar(255) not null,
+    expire_time datetime     null,
+    token       varchar(255) not null,
+    ID          int auto_increment
+    primary key
+    );
 
 create table if not exists users
 (
     name     varchar(255) not null,
     email    varchar(255) not null
-        primary key,
+    primary key,
     password varchar(255) not null
-);
-
+    );
